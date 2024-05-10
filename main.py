4fbd9672bdbb3f08676f190897648cb8e36eb403
IEnWu <iwu@stevens.edu>
2024-05-10 14:55:43 -0400

    main.py update

--- main.py
+++ main.py
@@ -261,26 +261,53 @@ def process_file(filepath):
     SMAC_KNN_model = joblib.load('SMAC_KNN_model.joblib')
     Tm_KNN_model = joblib.load('Tm_KNN_model.joblib')
 
-    prediction_ACSINS = ACSINS_SVR_model.predict(X_ACSINS)
-    prediction_AS = AS_LR_model.predict(X_AS)
-    prediction_BVP = BVP_KNN_model.predict(X_BVP)
-    prediction_CIC = CIC_KNN_model.predict(X_CIC)
-    prediction_CSI = CSI_SVR_model(X_CSI)
-    prediction_ELISA = ELISA_KNN_model(X_ELISA)
-    prediction_HIC = HIC_SVR_model(X_HIC)
-    prediction_HEK = HEK_KNN_model(X_HEK)
-    prediction_PSR = PSR_SVR_model.predict(X_PSR)
-    prediction_SGAC = SGAC_SVR_model.predict(X_SGAC)
-    prediction_SMAC = SMAC_KNN_model.predict(X_SMAC)
-    prediction_Tm = Tm_KNN_model(X_Tm)
+    ACSINS_transformed,AS,BVP,CIC_transformed,CSI_transformed,ELISA,HIC,HEK,PSR,SGAC_transformed,SMAC_transformed,Tm =[],[],[],[],[],[],[],[],[],[],[],[]
+    for index in dataset_pred.iterrows():
+
+        feature_ACSINS = X_ACSINS[index]
+        prediction_ACSINS = ACSINS_SVR_model.predict(feature_ACSINS.reshape(1,-1))
+        feature_AS = X_AS[index]
+        prediction_AS = AS_LR_model.predict(feature_AS.reshape(1,-1))
+        feature_BVP = X_BVP[index]
+        prediction_BVP = BVP_KNN_model.predict(feature_BVP.reshape(1,-1))
+        feature_CIC = X_CIC[index]
+        prediction_CIC = CIC_KNN_model.predict(feature_CIC.reshape(1,-1))
+        feature_CSI = X_CSI[index]
+        prediction_CSI = CSI_SVR_model.predict(feature_CSI.reshape(1,-1))
+        feature_ELISA = X_ELISA[index]
+        prediction_ELISA = ELISA_KNN_model.predict(feature_ELISA.reshape(1,-1))
+        feature_HIC = X_HIC[index]
+        prediction_HIC = HIC_SVR_model.predict(feature_HIC.reshape(1,-1))
+        feature_HEK = X_HEK[index]
+        prediction_HEK = HEK_KNN_model.predict(feature_HEK.reshape(1,-1))
+        feature_PSR = X_PSR[index]
+        prediction_PSR = PSR_SVR_model.predict(feature_PSR.reshape(1,-1))
+        feature_SGAC = X_SGAC[index]
+        prediction_SGAC = SGAC_SVR_model.predict(feature_SGAC.reshape(1,-1))
+        feature_SMAC = X_SMAC[index]
+        prediction_SMAC = SMAC_KNN_model.predict(feature_SMAC.reshape(1,-1))
+        feature_Tm = X_Tm[index]
+        prediction_Tm = Tm_KNN_model.predict(feature_Tm.reshape(1,-1))
+        ACSINS_transformed.append(prediction_ACSINS)
+        AS.append(prediction_AS)
+        BVP.append(prediction_BVP)
+        CIC_transformed.append(prediction_CIC)
+        CSI_transformed.append(prediction_CSI)
+        ELISA.append(prediction_ELISA)
+        HIC.append(prediction_HIC)
+        HEK.append(prediction_HEK)
+        PSR.append(prediction_PSR)
+        SGAC_transformed.append(prediction_SGAC)
+        SMAC_transformed.append(prediction_SMAC)
+        Tm.append(prediction_Tm)
 
     prediction_path = 'uploads/Biophysical_Prediction.csv'
-    df2 = pd.concat([pd.DataFrame(name_list), pd.DataFrame(prediction_ACSINS), pd.DataFrame(prediction_AS), pd.DataFrame(prediction_BVP), pd.DataFrame(prediction_CIC), pd.DataFrame(prediction_CSI), pd.DataFrame(prediction_ELISA), pd.DataFrame(prediction_HIC), pd.DataFrame(prediction_HEK), pd.DataFrame(prediction_PSR), pd.DataFrame(prediction_SGAC), pd.DataFrame(prediction_SMAC), pd.DataFrame(prediction_Tm)], ignore_index=True, axis=1)
-    df2.columns = ['Name', 'ACSINS_transformed', 'AS', 'BVP', 'CIC_transformed', 'CSI_transformed', 'ELISA', 'HIC', 'HEK', 'PSR', 'SGAC_transformed', 'SMAC_transformed', 'Tm']
-    df2.to_csv(prediction_path, index=False)
+    Name = dataset_pred[['Name']].to_numpy()
+    data = np.column_stack((Name,ACSINS_transformed,AS,BVP,CIC_transformed,CSI_transformed,ELISA,HIC,HEK,PSR,SGAC_transformed,SMAC_transformed,Tm))
+    np.savetxt(prediction_path, data, delimiter=',', fmt='%s', header='Name,ACSINS_transformed,AS,BVP,CIC_transformed,CSI_transformed,ELISA,HIC,HEK,PSR,SGAC_transformed,SMAC_transformed,Tm', comments='')
+
+    #df2 = pd.concat([pd.DataFrame(name_list), pd.DataFrame(prediction_ACSINS), pd.DataFrame(prediction_AS), pd.DataFrame(prediction_BVP), pd.DataFrame(prediction_CIC), pd.DataFrame(prediction_CSI), pd.DataFrame(prediction_ELISA), pd.DataFrame(prediction_HIC), pd.DataFrame(prediction_HEK), pd.DataFrame(prediction_PSR), pd.DataFrame(prediction_SGAC), pd.DataFrame(prediction_SMAC), pd.DataFrame(prediction_Tm)], ignore_index=True, axis=1)
+    #df2.columns = ['Name', 'ACSINS_transformed', 'AS', 'BVP', 'CIC_transformed', 'CSI_transformed', 'ELISA', 'HIC', 'HEK', 'PSR', 'SGAC_transformed', 'SMAC_transformed', 'Tm']
+    #df2.to_csv(prediction_path, index=False)
 
     return descriptors_path, prediction_path
-
-
-
-    
